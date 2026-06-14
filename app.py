bf1ef13cb0d4b2511fdce82c9a38a187e95861e8
ainsley215 <ainsley02121@gmail.com>
2026-06-14 20:51:17 +0700

    Fix blueprint duplication error

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def get_rekomendasi():
     return jsonify(hasil_df.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    # Jangan gunakan debug=True di production
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
