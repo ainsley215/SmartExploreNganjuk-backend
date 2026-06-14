9e2e4ba8d6f98cb57a97eef08f4fb572ea50d04b
ainsley215 <ainsley02121@gmail.com>
2026-06-14 21:16:23 +0700

    Fix blueprint duplication error

--- app.py
+++ app.py
@@ -34,3 +34,7 @@ def get_rekomendasi():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"message": "Backend SmartExplore Nganjuk Aktif!"}), 200
